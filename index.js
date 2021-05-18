ae5f4dc69613eba1a977296ee62a1cbee1b1fd71
zaqijr7 <zaqijr7@gmail.com>
2021-05-18 11:20:39 +0700

    https

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const dotenv = require('dotenv')
 dotenv.config()
 const { APP_PORT } = process.env
 const app = express()
-const server = require('http').createServer(app)
+const server = require('https').createServer(app)
 const io = require('socket.io')(server, {
   cors: {
     origin: '*'
